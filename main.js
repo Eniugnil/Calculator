3f3348d708de5ef7cbdf51550827c9997b2cac22
Eniugnil <138859684+Eniugnil@[200~users.noreply.github.com>
2023-09-12 13:17:54 -0700

    Add answer function and decimal compatability

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 function add(a, b) {
-    return a + b;
+    return +a + +b;
 }
 
 function subtract(a, b) {
-    return a - b;
+    return +a - +b;
 }
 
 function multiply(a, b) {
-    return a * b;
+    return +a * +b;
 }
 
 function divide(a, b) {
-    return a / b;
+    return +a / +b;
 }
 
 let numberOne;
 let operator;
 let numberTwo;
+let answeree = 1;
 
 function operate(num1, op, num2) {
     let total;
@@ -35,73 +36,149 @@ function operate(num1, op, num2) {
     }
 }
 
+//numberOne
 let containerOne = document.getElementById('calcScreen');
 let calcNumOne = document.createElement('div'); 
 containerOne.appendChild(calcNumOne);
 
 
+function addNumber(number) {
+    if (!numberOne) {
+        if (number === "+" || number === "-" || number === "*" || number === "/") {
+            //remember to display operator = number
+            operator = number;
+            numberOne = calcNumOne.textContent;
+            console.log(numberOne + " one");
+        }
+    }
+
+    const numericRegex = /^[0-9]+(\.[0-9]+)?$/;
+    if (numericRegex.test(number)|| number === ".") {
+        if (!numberOne) {
+            calcNumOne.textContent += number;
+        }
+    }
+    
+    if (numberOne) {
+        if (numberOne.length >= 1) {
+            numTwo(number)
+        }
+    }
+}
+
+//numberTwo
 let containerTwo = document.getElementById('calcScreen');
 let calcNumTwo = document.createElement('div'); 
 containerTwo.appendChild(calcNumTwo);
 
-function addNumber(number) {
-    if (!numberOne) {
-    calcNumOne.textContent += number;
-    }
-    
-    if (numberOne) {
-        if (numberOne.length > 1) {
-            numTwoTesting(number)
-        }
-    }
-    if (!numberOne) {
-        if (number === "+" || number === "-" || number === "*" || number === "/") {
-            numberOne = calcNumOne.textContent;
-            console.log(numberOne + "one");
-            }
-    }
-}
-
-function numTwoTesting(number) {
-        calcNumTwo.textContent += number;
-        if (number === "+" || number === "-" || number === "*" || number === "/") {
+function numTwo(number) {
+    if (!numberTwo) {
+        if (number === "+" || number === "-" || number === "*" || number === "/" || number === "=") {
         numberTwo = calcNumTwo.textContent;
-        console.log(numberTwo + "two");
+        console.log(numberTwo + " two");
         }
     }
 
+    const numericRegex = /^[0-9]+(\.[0-9]+)?$/;
+    if (numericRegex.test(number)|| number === ".") {
+        if (!numberTwo) {
+            calcNumTwo.textContent += number;
+        }
+    }
 
-    
-
-
-
-    // if (numberOne) {
-    //   if (numberOne.length > 1) {
-    //     calcNumTwo.textContent += number;
-    //     numberTwo = calcNumTwo.textContent;
-    //     console.log(numberTwo + " two");
-    //   }
-    // }
-  
-    
-
-    // if (numberOne && numberTwo) {
-    //     if (number === "=") {
-    //         console.log("hello")
-    //     }
-    
-    // }
-
-
-//delete buttons are non working properly
-function clearAll() {
-    calcNumOne.textContent = "";
-}
-
-function deleteOne() {
-    let currentText = calcNumOne.textContent;
-    if (currentText.length > 0) {
-        let newText = currentText.slice(0, -1);
-        calcNumOne.textContent = newText;
+    if (numberTwo) {
+        if (numberTwo.length >= 1) {
+            operateTest(operator)
+        }
     }
 }
+
+let containerAns = document.getElementById('calcScreen');
+let calcNumAns = document.createElement('div'); 
+containerTwo.appendChild(calcNumAns);
+
+function operateTest(mathOperator) {
+//(*)figure the logic when someone puts in an operator instead of an = sign
+
+//if they click an operator solve and that answer as num1
+//clear num2 so it can be reused
+//make it run back to num2 function to continue
+
+//make the answer use what the operator was. Not the one you're going to input next
+
+
+    if (mathOperator === "+") {
+        answeree = add(numberOne, numberTwo);
+        numberOne = answeree;
+        containerOne.textContent = answeree;
+    }
+
+    if (mathOperator === "-") {
+        answeree = subtract(numberOne, numberTwo);
+        numberOne = answeree;
+        containerOne.textContent = answeree;
+    }
+
+    if (mathOperator === "*") {
+        answeree = multiply(numberOne, numberTwo);
+        numberOne = answeree;
+        containerOne.textContent = answeree;
+    }
+
+    if (mathOperator === "/") {
+        answeree = divide(numberOne, numberTwo);
+        numberOne = answeree;
+        containerOne.textContent = answeree;
+    }
+
+    if (mathOperator === "=") {
+        if (operator == "+") {
+            answeree = add(numberOne, numberTwo);
+            calcNumAns.textContent = add(numberOne, numberTwo);
+
+        }
+
+        if (operator == "-") {
+            answeree = subtract(numberOne, numberTwo);
+            calcNumAns.textContent = subtract(numberOne, numberTwo);
+
+        }
+
+        if (operator == "*") {
+            answeree = multiply(numberOne, numberTwo);
+            calcNumAns.textContent = multiply(numberOne, numberTwo);
+
+        }
+
+        if (operator == "/") {
+            answeree = divide(numberOne, numberTwo);
+            calcNumAns.textContent = divide(numberOne, numberTwo);
+
+        }
+    }
+}
+
+        
+
+
+        // mathOperator === "-"
+
+        // mathOperator === "*"
+
+        // mathOperator === "/" 
+
+    
+//The clear button breaks the calculator after clicked. Numbers can't be inputted
+// function clearAll() {
+//     calcNumOne.textContent = "";
+//     calcNumTwo.textContent = "";
+// }
+
+//delete button is currently not working
+// function deleteOne() {
+//     let currentText = calcNumOne.textContent;
+//     if (currentText.length > 0) {
+//         let newText = currentText.slice(0, -1);
+//         calcNumOne.textContent = newText;
+//     }
+// }
