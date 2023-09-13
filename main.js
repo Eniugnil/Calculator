f1369d8a356ddbebbccd2c29a41e45585ce7102e
Eniugnil <138859684+Eniugnil@[200~users.noreply.github.com>
2023-09-13 10:50:10 -0700

    Fix operator using latest operator chosen

--- main.js
+++ main.js
@@ -45,8 +45,8 @@ containerOne.appendChild(calcNumOne);
 function addNumber(number) {
     if (!numberOne) {
         if (number === "+" || number === "-" || number === "*" || number === "/") {
-            //remember to display operator = number
             operator = number;
+            ope.textContent = operator
             numberOne = calcNumOne.textContent;
             console.log(numberOne + " one");
         }
@@ -66,6 +66,11 @@ function addNumber(number) {
     }
 }
 
+//operator (also remember to just combine all the containers from num1,ope,num2,ans)
+let containerOpe = document.getElementById('calcScreen');
+let ope = document.createElement('div'); 
+containerOpe.appendChild(ope);
+
 //numberTwo
 let containerTwo = document.getElementById('calcScreen');
 let calcNumTwo = document.createElement('div'); 
@@ -73,114 +78,57 @@ containerTwo.appendChild(calcNumTwo);
 
 function numTwo(number) {
     if (!numberTwo) {
-        if (number === "+" || number === "-" || number === "*" || number === "/" || number === "=") {
-        numberTwo = calcNumTwo.textContent;
-        console.log(numberTwo + " two");
+        if (number === "+" || number === "-" || number === "*" || number === "/" ) {
+            numberTwo = calcNumTwo.textContent;
+            if (operator === "+") {
+                answeree = add(numberOne, numberTwo);
+                numberOne = answeree;
+                calcNumOne.textContent = answeree;
+                numberTwo = "";
+                calcNumTwo.textContent = "";
+                operator = number;
+                ope.textContent = operator
+            } else if (operator === "-") {
+                answeree = subtract(numberOne, numberTwo);
+                numberOne = answeree;
+                calcNumOne.textContent = answeree;
+                numberTwo = "";
+                calcNumTwo.textContent = "";
+                operator = number;
+                ope.textContent = operator
+            } else if (operator === "*") {
+                answeree = multiply(numberOne, numberTwo);
+                numberOne = answeree;
+                calcNumOne.textContent = answeree;
+                numberTwo = "";
+                calcNumTwo.textContent = "";
+                operator = number;
+                ope.textContent = operator
+            } else if (operator === "/") {
+                answeree = divide(numberOne, numberTwo);
+                numberOne = answeree;
+                calcNumOne.textContent = answeree;
+                numberTwo = "";
+                calcNumTwo.textContent = "";
+                operator = number;
+                ope.textContent = operator
+            }
         }
     }
 
     const numericRegex = /^[0-9]+(\.[0-9]+)?$/;
     if (numericRegex.test(number)|| number === ".") {
-        if (!numberTwo) {
+        if (!numberTwo || numberTwo === "") {
             calcNumTwo.textContent += number;
         }
     }
-
-    if (numberTwo) {
-        if (numberTwo.length >= 1) {
-            operateTest(operator)
-        }
-    }
 }
 
-let containerAns = document.getElementById('calcScreen');
-let calcNumAns = document.createElement('div'); 
-containerTwo.appendChild(calcNumAns);
-
-function operateTest(mathOperator) {
-//(*)figure the logic when someone puts in an operator instead of an = sign
-
-//if they click an operator solve and that answer as num1
-//clear num2 so it can be reused
-//make it run back to num2 function to continue
-
-//make the answer use what the operator was. Not the one you're going to input next
-
-
-    if (mathOperator === "+") {
-        answeree = add(numberOne, numberTwo);
-        numberOne = answeree;
-        calcNumOne.textContent = answeree;   //this is the text content and not the actual variable storing the information
-        calcNumTwo.textContent = "";
-        numberTwo = "";
-        answeree = "";
-
-    }
-
-    if (mathOperator === "-") {
-        answeree = subtract(numberOne, numberTwo);
-        numberOne = answeree;
-        calcNumOne.textContent = answeree;
-        calcNumTwo.textContent = "";
-
-    }
-
-    if (mathOperator === "*") {
-        answeree = multiply(numberOne, numberTwo);
-        numberOne = answeree;
-        calcNumOne.textContent = answeree;
-        calcNumTwo.textContent = "";
-
-    }
-
-    if (mathOperator === "/") {
-        answeree = divide(numberOne, numberTwo);
-        numberOne = answeree;
-        calcNumOne.textContent = answeree;
-        calcNumTwo.textContent = "";
-    }
-
-    if (mathOperator === "=") {
-        if (operator == "+") {
-            answeree = add(numberOne, numberTwo);
-            calcNumAns.textContent = add(numberOne, numberTwo);
-
-        }
-
-        if (operator == "-") {
-            answeree = subtract(numberOne, numberTwo);
-            calcNumAns.textContent = subtract(numberOne, numberTwo);
-
-        }
-
-        if (operator == "*") {
-            answeree = multiply(numberOne, numberTwo);
-            calcNumAns.textContent = multiply(numberOne, numberTwo);
-
-        }
-
-        if (operator == "/") {
-            answeree = divide(numberOne, numberTwo);
-            calcNumAns.textContent = divide(numberOne, numberTwo);
-
-        }
-    }
-}
-
-        
-
-
-        // mathOperator === "-"
-
-        // mathOperator === "*"
-
-        // mathOperator === "/" 
-
-    
 function clearAll() {
 
     //this is the text content and not the actual variable storing the information
     calcNumOne.textContent = "";
+    ope.textContent = "";
     calcNumTwo.textContent = "";
     calcNumAns.textContent = "";
 
@@ -190,12 +138,3 @@ function clearAll() {
     numberTwo = "";
     answeree = "";
 }
-
-//delete button is currently not working
-// function deleteOne() {
-//     let currentText = calcNumOne.textContent;
-//     if (currentText.length > 0) {
-//         let newText = currentText.slice(0, -1);
-//         calcNumOne.textContent = newText;
-//     }
-// }
